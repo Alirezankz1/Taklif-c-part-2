506bce134b3161e295c33d6c8626c26dd7299550
Alirezankz1 <117285111+Alirezankz1@users.noreply.github.com>
2022-11-16 19:10:07 +0330

    2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,65 @@
 ﻿using System;
-class test {
-public static void Main(){
-string x = Console.ReadLine();
-int a = Convert.ToInt32(x);
-Console.WriteLine(tavan_2(a));
-Console.WriteLine(tavan_3(a));
-}
-private static double tavan_2(int a)
+class test{
+
+public static void Main()
 {
-double y = Math.Pow(a,2);
-return (y);
+string a = Console.ReadLine();
+int x = Convert.ToInt32(a);
+Console.Write(year(x));
+Console.Write("/");
+Console.Write(Month(x));
+Console.Write("/");
+Console.Write(week(x));
+Console.Write("/");
+Console.Write(day(x));
+Console.Write("/");
+Console.WriteLine(x);
+Console.Write(year(x));
+Console.Write("/");
+int y = x - (int)year(x) * 360;
+Console.Write(Month(x));
+Console.Write("/");
+int m = y - (int)Month(x) * 30;
+Console.Write(week(x));
+Console.Write("/");
+int w = m - (int)(week(x)) * 7;
+Console.Write(day(x));
+Console.Write("/");
+int p = w - (int)(day(x)) * 1;
+Console.WriteLine(p);
 
 }
-private static double tavan_3(int a){
 
-double y  = Math.Pow(a,3);
-return y;
+private static int year(int x){
+
+return (int)(x / 360);
+
+}
+
+private static int Month(int x){
+
+
+return (int)(x / 30);
+
 
 }
 
+
+private static int week(int x){
+
+return (int)(x / 7);
+
+
 }
+
+private static int day(int x){
+
+return (int)(x / 1);
+
+
+}
+
+
+}
+
+
