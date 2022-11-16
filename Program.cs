9e26186387978c837a42cf3092326efceed87190
Alirezankz1 <117285111+Alirezankz1@users.noreply.github.com>
2022-11-16 20:20:07 +0330

    4

--- Program.cs
+++ Program.cs
@@ -1,37 +1,72 @@
 ﻿using System;
-class test{
+using System.ComponentModel;
 
-public static void Main(){
-
-Console.WriteLine("mojodi:");
-string a = Console.ReadLine();
-Console.WriteLine("bahre:");
-string b = Console.ReadLine();
-Console.WriteLine("mojodi jadid:");
-string c = Console.ReadLine();
-int x = Convert.ToInt32(a);
-double y = Convert.ToDouble(b);
-double z = Convert.ToInt32(c);
-Console.WriteLine("sal:");
-Console.WriteLine(mohasehat(x,y,z));
-}
-private static int mohasehat(int  x , double y , double z );
+class test
 {
- double a = 0;
- int b=0; 
- for(int i = 1; x < z; i++);
- {
-a = x * (y/100);
-x = (int)(x + a);
-b = i;
- }
- return b;
-
+    public static void Main()
+    {
+        int temp = 0;
+        
+        string a = Console.ReadLine();
+        if (a == "0")
+        {
+            
+            a = Console.ReadLine();
+        }
+      
+        string b = Console.ReadLine();
+        if (b == "0")
+        {
+            
+            b = Console.ReadLine();
+        }
+        int x = Convert.ToInt32(a);
+        int y = Convert.ToInt32(b);
+            if (y > x)
+            {
+                temp = y;
+                y = x;
+                x = temp;
+            }
+        Console.Write("KMM=");
+        Console.WriteLine(KMM(x, y));
+        Console.Write("BMM=");
+        Console.Write(BMM(x, y));
+    }
+    private static double KMM(int x, int y)
+    {
+        double m =0;
+        int n = 0;
+        do
+        {
+            n++;
+            m = x * n;
+        }
+        while (m % y != 0);
+        return m;
+    }
+    private static double BMM(int x, int y)
+    {
+        int m = 0;
+        int n = 0;
+        int p = 0;
+        do
+        {
+            m++;
+            if (x % m == 0)
+            {
+                n = x / m;
+                if (y % n == 0)
+                {
+                    p = n;
+                }
+            }
+        }
+        while ((y % m) == 0);
+        if(p == 0)
+        {
+            Console.Write("BMM nadarad    ");
+        }
+            return (p);
+    }
 }
-
-
-}
- 
-
-
-
