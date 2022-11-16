8620796b6c7d976c0883d7306c131a355d6914df
Alirezankz1 <117285111+Alirezankz1@users.noreply.github.com>
2022-11-16 18:12:56 +0330

    1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class test {
+public static void Main(){
+string x = Console.ReadLine();
+int a = Convert.ToInt32(x);
+Console.WriteLine(tavan_2(a));
+Console.WriteLine(tavan_3(a));
+}
+private static double tavan_2(int a)
+{
+double y = Math.Pow(a,2);
+return (y);
+
+}
+private static double tavan_3(int a){
+
+double y  = Math.Pow(a,3);
+return y;
+
+}
+
+}
